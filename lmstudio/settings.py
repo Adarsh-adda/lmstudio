4e5f764f12c0f420120b2fc7bbf32193ac7d1ce3
Adarsh-adda <adarshku123654@gmail.com>
2022-11-11 19:22:32 +0530

    ready for production

--- lmstudio/settings.py
+++ lmstudio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a9r4grl-1e#octvybr94=o!@kvmr$7*61(y&!pt9blq64#2@=y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
